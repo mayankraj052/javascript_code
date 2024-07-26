a79736b0d9f11041aa5d8feb82fe1fbc6dfa69a6
Mayank Raj <111748811+mayankraj052@users.noreply.github.com>
2024-07-26 05:05:52 +0000

    dataTypes in js

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict" // treat all js code as newer version
+
+// alert("hello") // we are using nodejs not web browser
+
+console.log(3 
+    + 
+    3) //code readability should be high
+
+console.log("Mayank")
+
+
+let name = "mayank"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//  ** Primitive Data type ** 
+//numbert => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => stabdalone value
+//undefined =>
+//symbol => to find uniqueness 
+
+// ** object **
+
+
+console.log(typeof("mayank"))
+console.log(typeof(null)) //=> object
+console.log(typeof(undefined)) // => undefined
+//node 01_basic/02_dataTypes.js
