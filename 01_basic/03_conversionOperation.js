3cb9b1b87664b39443b205904938431637f44a36
Mayank Raj <111748811+mayankraj052@users.noreply.github.com>
2024-08-24 16:21:19 +0000

    conversion done

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -26,7 +26,42 @@
  */
 
 
-let number = 33
-let stringNumber = String(number)
-console.log(typeof(stringNumber))
-console.log(stringNumber)
+// let number = 33
+// let stringNumber = String(number)
+// console.log(typeof(stringNumber))
+// console.log(stringNumber)
+// ######### Operations #########
+
+// let value =3
+// let negValue = -value;
+// // console.log(negValue); //-> 3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2); // -> power
+// console.log(2%2);
+
+
+// let str1 = "mayank"
+// let str2 = " Raj"
+// console.log(str1 + str2);
+// console.log(1 + "2") // 12
+// console.log("1" + 2) // 12
+// console.log("1" + 2 +2) // 122  -> "12" + 2 -> 122
+// console.log(1 + 2 +"2") // 32 -> 3 +"2" -> 32
+
+// console.log(true)// true
+// console.log(+true) // 1
+
+// console.log(+"") // -> 0
+// console.log("") // -> 
+
+
+// let num1, num2,num3
+// num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
