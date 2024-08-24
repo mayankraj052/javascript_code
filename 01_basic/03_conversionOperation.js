75bc78f873fae1ccbe3eb51cfcaa022cc71ba88c
Mayank Raj <111748811+mayankraj052@users.noreply.github.com>
2024-08-24 16:01:11 +0000

    conversion operation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,1 +1,32 @@
-//   node 01_basic/02_conversionOperation.js
+// //   node 01_basic/02_conversionOperation.js
+// let score = "mayank";
+// console.log(typeof(score));
+// let valueNumber = Number(score);
+// console.log(typeof(valueNumber));
+// console.log(valueNumber);
+
+// /*
+// "33" -> 33
+// "33ab" -> Nan
+// true -> 1
+// false -> 0
+// "sanu" -> Nan
+// undefined -> Nan
+// */
+
+
+// let isLoggedIn = "sanu"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn) 
+/*
+1 -> true
+0 -> false
+"" -> false
+"sanu" -> true
+ */
+
+
+let number = 33
+let stringNumber = String(number)
+console.log(typeof(stringNumber))
+console.log(stringNumber)
